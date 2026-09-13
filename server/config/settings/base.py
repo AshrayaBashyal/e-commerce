5bb0ef0dd5f4226ac576ee211998ce5fa9b04dce
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2026-09-13 17:34:20 +0545

    add async drf in insalled_apps

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'django_filters',
+    'adrf',  # async support on top of DRF
 ]
 
 MIDDLEWARE = [
