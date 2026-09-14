2d148f1cdfced6905b6f4476e341466871975956
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2026-09-14 07:09:20 +0545

    register endpoints forregister, login and refresh

--- server/apps/accounts/urls.py
+++ server/apps/accounts/urls.py
@@ -1,12 +1,19 @@
 from adrf.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+ 
+from django.urls import include, path
+ 
+from .views import AddressViewSet, MeView, RegisterView
 
-from django.urls import path
-
-from .views import AddressViewSet, MeView
-
+ 
 router = DefaultRouter()
 router.register('addresses', AddressViewSet, basename='address')
-
+ 
 urlpatterns = [
-    path('me/', MeView.as_view(), name='me'),
-] + router.urls
+    path('auth/register/', RegisterView.as_view(), name='register'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+ 
+    path('accounts/me/', MeView.as_view(), name='me'),
+    path('accounts/', include(router.urls)),
+]
