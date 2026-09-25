147b831538ff94ff17f3b78f9b0f292a000d5aeb
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2026-09-25 13:36:33 +0545

    Configure dj-database-url for dynamic database setup

--- server/config/settings.py
+++ server/config/settings.py
@@ -67,12 +67,15 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 ASGI_APPLICATION = "config.asgi.application"
 
+
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+    )
 }
+
 
 AUTH_USER_MODEL = "users.User"
 
